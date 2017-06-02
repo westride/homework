cca45d901507bcf35912777c56f25750303310a8
Amy Zhang <amyzh25@hotmail.com>
2017-06-02 19:58:34 -0400

    added

--- homework-3/hw3.rb
+++ homework-3/hw3.rb
@@ -8,41 +8,37 @@ class Utility
       end
 
 
-
        result
     end
 end
 
-  puts  Utility.new.count_words('it was the best of times, it was the worst of times.')
+puts  Utility.new.count_words('it was the best of times, it was the worst of times')
 
- # Problem 2
- # class Animal
- #    def sound
- #      puts  animal sound
- #  end
+ Problem 2
+ class Animal
+  def sound
+      puts  animal sound
+  end
 
+  def speak
+    puts "The animal says:#{sound}"
+  end
+end
 
- #  problem 4: "get" turns URL into the website, whereas "post" turns the data from
- #  the users into another page of website.
- #    def speak
- #    puts "The animal says:#{sound}"
+class Cow < Animal
+  def sound
+   puts "moo"
+  end
+end
 
- #    end
- #  end
-
- #  class Cow < Animal
- #     def sound
- #     "moo"
- #    end
- #  end
- #  class Duck < Animal
- #     def sound
- #    puts "Quack"
- #  end
- #  end
+class Duck < Animal
+  def sound
+    puts "Quack"
+  end
+end
 
 
- #  cow=Cow.new.sound
- #  cow1=Cow.new.speak
- #  duck=Duck.new.sound
- #  duck1=Duck.new.speak
+cow=Cow.new.sound
+cow1=Cow.new.speak
+duck=Duck.new.sound
+duck1=Duck.new.speak
