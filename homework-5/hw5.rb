77c86f5e2b661256d1b5b5e57e43bbe3bbe6a6d1
Amy Zhang <amyzh25@hotmail.com>
2017-06-08 13:52:06 -0400

    add hw5

--- homework-5/hw5.rb
+++ homework-5/hw5.rb
@@ -1,7 +1,21 @@
 #hw5. 1
 
-first_name: string
- last_name: string
- create_at: datetime
-  is_admin: boolean
-#2.
+name: string
+post:text
+age: integer
+blurb:string
+
+
+
+2. User.create(name:"amy", age:32,blurb:"journey")
+    User.create(name:Elisa",age:25)
+    User.last.update(blurb:"today's treat")
+    User.find(1)
+    User.all.collect t{|u| puts u.name}
+    User.first
+    User.first.update(name:"Cindy")
+    User.last
+    User.create(name:"edward")
+    User.last.update(age:28)
+    User.destroy
+
